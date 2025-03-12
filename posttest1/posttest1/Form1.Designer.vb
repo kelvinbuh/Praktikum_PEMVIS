e4fa60b2af62f61ad3157427dfb3121a3409b8a9
kelvinbuh <162068546+kelvinbuh@users.noreply.github.com>
2025-03-12 11:08:30 +0800

    Add files via upload

---
+++ posttest1/posttest1/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        GroupBox1 = New GroupBox()
+        Label3 = New Label()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        Button2 = New Button()
+        Button1 = New Button()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(32, 19)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "IP Semester"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(144, 19)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(163, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Button2)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(248, 121)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(361, 271)
+        GroupBox1.TabIndex = 2
+        GroupBox1.TabStop = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(202, 137)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Label3"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(146, 101)
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(161, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(32, 98)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(72, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(232, 48)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(144, 48)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Tambah"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ posttest1/posttest1/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.ReadOnly = True
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim IPSemester As Double
+
+
+        If Double.TryParse(TextBox1.Text, IPSemester) Then
+            If IPSemester >= 0 And IPSemester <= 4 Then
+                totalIP += IPSemester
+                jumlahSemester += 1
+
+                Dim ipk As Double = totalIP / jumlahSemester
+                TextBox2.Text = ipk.ToString("0.00")
+
+                If ipk >= 2 AndAlso ipk <= 2.75 Then
+                    Label3.Text = "Cukup"
+                ElseIf ipk > 2.75 AndAlso ipk <= 3.0 Then
+                    Label3.Text = "Memuaskan"
+                ElseIf ipk > 3.0 Then
+                    Label3.Text = "sangat memuaskan"
+                Else
+                    Label3.Text = ""
+                End If
+
+                TextBox1.Text = ""
+            Else
+                MessageBox.Show("Masukkan IP semester antara 0  hingga 4", "Input tidak valid", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        Else
+            MessageBox.Show("Masukkan angka yang valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        totalIP = 0
+        jumlahSemester = 0
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        Label3.Text = ""
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
